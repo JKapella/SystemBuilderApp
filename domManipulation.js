9efd237aaff47aa0f23cbdb23fb74bb3c9c48a87
Jkapella <jvkapella@gmail.com>
2019-04-13 11:32:24 +0100

    refactored star printing html

--- domManipulation.js
+++ domManipulation.js
@@ -2,22 +2,13 @@ function outputHTMLTables(createdSystem) {
     document.getElementById('systemDetails').innerHTML = ''
     var systemDetailsHTML = ''
     systemDetailsHTML += createSystemNameHTML(createdSystem.name)
-    // addStarDetailstoHTMLTable(createdSystem.createdStar)
+    systemDetailsHTML += addStarDetailsHTML(createdSystem.createdStar)
     // addPlanetDetailsToHTMLTable(createdSystem)
 
     document.getElementById('systemDetails').innerHTML = systemDetailsHTML
 }
 
-//     <table id="mainTable">
-//     <tr>
-//     <th>Star Name</th>
-// <th>Mass (M⊕)</th>
-// <th>Age (bya)</th>
-// <th>Size (R⊕)</th>
-// <th>Type (Stellar Class)</th>
-// </tr>
-// <tr id="starDetails"></tr>
-//     </table>
+//
 //     <h4>Planets</h4>
 //     <table id="planetDetails">
 //     <tr>
@@ -33,19 +24,24 @@ function createSystemNameHTML(starName) {
 }
 
 function addStarDetailsHTML(starInfo) {
-
-
-
-
-    //create the cell
-    var starInfoCell = document.createElement("td");
-    starInfoCell.setAttribute("class", "generatedTable");
-    //create the content for the cell
-    var content = document.createTextNode(info);
-    //add content to the cell
-    starInfoCell.appendChild(content);
-    //add the cell to the doc
-    document.getElementById("starDetails").appendChild(starInfoCell);
+    var html =
+`<table>
+    <tr>
+        <th>Star Name</th>
+        <th>Mass (M&#8853;)</th>
+        <th>Age (bya)</th>
+        <th>Size (R&#8853;)</th>
+        <th>Type (Stellar Class)</th>
+    </tr>
+    <tr>
+        <td>${starInfo.name}</td>
+        <td>${starInfo.mass}</td>
+        <td>${starInfo.age}</td>
+        <td>${starInfo.size}</td>
+        <td>${starInfo.type}</td>
+    </tr> 
+</table>`
+    return html
 }
 
 //TODO This function and the star details function can be merged (maybe?)
@@ -73,13 +69,7 @@ function addPlanetDetailsHTML(planetName,PlanetOrbit,PlanetYear) {
     document.getElementById(planetName).appendChild(planetYearCell);
 }
 
-function addStarDetailstoHTMLTable(star) {
-    addStarDetailsHTML(star.name);
-    addStarDetailsHTML(star.mass);
-    addStarDetailsHTML(star.age);
-    addStarDetailsHTML(star.size);
-    addStarDetailsHTML(star.type);
-}
+
 
 function addPlanetDetailsToHTMLTable(system) {
     for (var currentPlanet = 0; currentPlanet < system.noOfPlanets; currentPlanet++) {
